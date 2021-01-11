9ae7daa888432c979a9a8d501cf27c7b5c82765b
MochizukiShunju <b1302475@gmail.com>
2021-01-11 11:37:56 +0000

    [課題２]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,13 @@
 class BooksController < ApplicationController
   def top
   end
-  
+
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
@@ -15,7 +17,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path
+    redirect_to book_path(book)
   end
 
   def edit
@@ -24,7 +26,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :category, :body)
+    params.permit(:title, :body)
   end
 end
 
