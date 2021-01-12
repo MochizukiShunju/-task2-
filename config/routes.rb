ca9b2c5821aca6889177c487c7d9e0faf9b1d8f0
MochizukiShunju <b1302475@gmail.com>
2021-01-12 10:32:30 +0000

    [課題２]追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'books#top'
   resources :books
-  # get 'books/:id' => 'books#show'
+  # get 'books/:id' => 'books#show',as: 'book'
 end
